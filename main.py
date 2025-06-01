57f788f5f7f6d5f59b46ba900879fa090df7405a
Bagus <bagusknusantara@gmail.com>
2025-06-01 21:50:46 +0700

    Adding get_char_dict function

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from stats import get_num_words, get_chars_dict
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = len(text.split())
+    num_words = get_num_words(text)
+    num_chars = get_chars_dict(text)
     print(f"{num_words} words found in the document")
+    print(num_chars)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
 
 main()
---
+++ stats.py
@@ -0,0 +1,14 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    dict = {}
+    for char in text:
+        char = char.lower()
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+            
