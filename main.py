ba7e4eb4d40ebbf0fae3a14373a0f6b38ff0f948
Bagus <bagusknusantara@gmail.com>
2025-06-01 22:33:07 +0700

    Adding sorting alphabet dictionary function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, get_sorted_alpha_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_chars = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
-
+    chars_dict = get_chars_dict(text)
+    sorted_alpha_dict = get_sorted_alpha_dict(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in sorted_alpha_dict:
+        print(f"{char}: {sorted_alpha_dict[char]}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -11,4 +11,13 @@ def get_chars_dict(text):
         else:
             dict[char] = 1
     return dict
+
+def get_sorted_alpha_dict(chars_dict):
+    alpha_dict = {}
+    for char in chars_dict:
+        if char.isalpha():
+            alpha_dict[char] = chars_dict[char]
+    sorted_dict = dict(sorted(alpha_dict.items(), key=lambda item: item[1], reverse=True))
+    return sorted_dict
+
             
